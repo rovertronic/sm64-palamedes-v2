11274b430c1c7062f605ff3a166fdeeaa277e83c
rovertronic <rovertentertainment@gmail.com>
2025-10-09 12:25:02 -0500

    Star Switch Event... needs more granular cutscene control in mian though

--- src/game/behaviors/mop.inc.c
+++ src/game/behaviors/mop.inc.c
@@ -1,6 +1,27 @@
 /* Star Switch from SM64 DS*/
-void bhv_star_switch(void) {
+EventData eventStarSwitch[] = {
+    E_CINEMA_TOGGLE(),
+    E_DIALOG(0),
+    E_END(),
+};
 
+void bhv_star_switch(void) {
+    struct Object * myStarGlass = cur_obj_nearest_object_with_behavior(bhvStarSwitchStar);
+    if (myStarGlass == NULL) {return;}
+
+    switch(o->oAction) {
+        case 0:
+            if (gMarioObject->platform == o) {
+                o->oAction++;
+                event_start(eventStarSwitch);
+            }
+            break;
+        case 1:
+            if (o->oPosY > o->oHomeY - 20.0f) {
+                o->oPosY-=2.0f;
+            }
+            break;
+    }
 }
 
 void bhv_star_switch_star(void) {
